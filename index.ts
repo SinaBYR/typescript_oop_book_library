40692e6cc09a793dbaf90d5599c789e3da89cc6f
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-05-31 01:42:19 +0330

    fix: change getter names

--- index.ts
+++ index.ts
@@ -7,26 +7,26 @@
  */
 
 class Book {
-	public name: string
-	public page_number: number
-	public edition: number
+	public _name: string
+	public _page_number: number
+	public _edition: number
 
 	constructor(name: string, page_number: number, edition: number = 1) {
-		this.name = name
-		this.page_number = page_number
-		this.edition = edition
+		this._name = name
+		this._page_number = page_number
+		this._edition = edition
 	}
 
-	get getName() {
-		return this.name
+	get name() {
+		return this._name
 	}
 
-	get getEdition() {
-		return this.edition
+	get edition() {
+		return this._edition
 	}
 
-	get getPageNumber() {
-		return this.page_number
+	get pageNumber() {
+		return this._page_number
 	}
 }
 
